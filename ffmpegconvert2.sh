ad71a8f7bb052e4147ac481ac3d1398a3328c5f1
iam-eo <ian.eric.ong@gmail.com>
2022-12-31 08:20:24 +0800

    Create ffmpegconvert2.sh

---
+++ ffmpegconvert2.sh
@@ -0,0 +1,25 @@
+shopt -s globstar nullglob dotglob
+
+#mkdir original
+for file in ./**/*.{mpg,mpeg,mp4,avi}
+do
+
+
+dirPath="${file%/*}/encoded"
+mkdir -p "${dirPath}"
+
+oldname="${file}"
+fileext="${file##*.}"
+newname="${oldname%.$fileext}_convert.$fileext"
+
+mv "$oldname" "$newname"
+
+
+ffmpeg -i "$newname" -c:a copy -s 1280:720 "${oldname%.$fileext}".mp4
+
+mv "${oldname%.$fileext}".mp4 "$dirPath"
+mv "$newname" "$oldname"
+
+
+#ffmpeg -i "$file" -c:a aac -b:a 128k "encoded/${file%.*}".mp4
+done
