e5ced7c1e104b32a5f3fc0a1e5190c29b2858e49
iam-eo <ian.eric.ong@gmail.com>
2023-01-03 02:45:36 +0800

    Update ffmpegconvert.sh

--- ffmpegconvert.sh
+++ ffmpegconvert.sh
@@ -3,21 +3,19 @@ shopt -s globstar nullglob dotglob
 for file in ./**/*.{mpg,mpeg,mp4,avi}
 do
 
-
 dirPath="${file%/*}/encoded"
 mkdir -p "${dirPath}"
 
 oldname="${file}"
 fileext="${file##*.}"
+
 newname="${oldname%.$fileext}_convert.$fileext"
 
 mv "$oldname" "$newname"
 
-
-ffmpeg -i "$newname" -c:a copy -s 1280:720 "${oldname%.$fileext}".mp4
+ffmpeg -i "$newname" -c:a copy -s 1280:720 "${oldname%.$fileext}".mp4 > output.txt 2>&1
 
 mv "${oldname%.$fileext}".mp4 "$dirPath"
 mv "$newname" "$oldname"
-
 
 done
