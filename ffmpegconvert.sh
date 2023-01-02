05305ee17c62999d5070afece281a579b35d968a
iam-eo <ian.eric.ong@gmail.com>
2023-01-03 02:48:53 +0800

    Create ffmpegconvert.sh

--- ffmpegconvert.sh
+++ ffmpegconvert.sh
@@ -13,7 +13,7 @@ newname="${oldname%.$fileext}_convert.$fileext"
 
 mv "$oldname" "$newname"
 
-ffmpeg -i "$newname" -c:a copy -s 1280:720 "${oldname%.$fileext}".mp4 > output.txt 2>&1
+ffmpeg -i "$newname" -c:a copy -s 1280:720 "${oldname%.$fileext}".mp4 >> output.txt 2>&1
 
 mv "${oldname%.$fileext}".mp4 "$dirPath"
 mv "$newname" "$oldname"
