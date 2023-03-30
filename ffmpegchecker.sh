b9a3e198a2d08381d307c5f61c753bf2db3b6830
iam-eo <ian.eric.ong@gmail.com>
2023-03-31 06:12:55 +0800

    Update ffmpegchecker.sh

--- ffmpegchecker.sh
+++ ffmpegchecker.sh
@@ -1,8 +1,9 @@
+ 
 shopt -s globstar nullglob dotglob
 
 rm -f error.log
 
-for file in ./**/*.{mpg,mpeg,mp4,MP4,avi,flv,mkv}
+for file in ./**/*.{mpg,mpeg,mp4,MP4,avi,flv,mkv,mov}
 do
 
 echo "$file"
