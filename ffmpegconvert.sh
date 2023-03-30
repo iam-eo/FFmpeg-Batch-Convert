1cc466785900705ff918b88a7c8f7083080f0a6b
iam-eo <ian.eric.ong@gmail.com>
2023-03-31 06:12:32 +0800

    Update ffmpegconvert.sh

--- ffmpegconvert.sh
+++ ffmpegconvert.sh
@@ -1,8 +1,8 @@
 shopt -s globstar nullglob dotglob
 
-output.txt
+rm -f output.txt
 
-for file in ./**/*.{mpg,mpeg,mp4,avi,mov}
+for file in ./**/*.{mpg,mpeg,mp4,avi.mov}
 do
 
 dirPath="${file%/*}/encoded"
@@ -18,6 +18,9 @@ newname="${oldname%.$fileext}_convert.$fileext"
 mv "$oldname" "$newname"
 
 ffmpeg -i "$newname" -c:a copy -s 1280:720 "${oldname%.$fileext}".mp4 >> output.txt 2>&1
+#ffmpeg -i "$newname" -c:a copy -s 1920:1080 "${oldname%.$fileext}".mp4 >> output.txt 2>&1
+
+
 
 mv "${oldname%.$fileext}".mp4 "$dirPath"
 mv "$newname" "$oldname"
